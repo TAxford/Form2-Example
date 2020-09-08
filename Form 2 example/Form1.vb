35c72564242d9c0e5adafedecd35e322bd6cf787
Timothy Axford <35697045+TAxford@users.noreply.github.com>
2020-09-08 06:40:54 -0400

    Added second form as well as button action codes to open and close forms.

--- Form 2 example/Form1.vb
+++ Form 2 example/Form1.vb
@@ -2,4 +2,12 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Form2.show()
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.AddOwnedForm(form2)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.RemoveOwnedForm(form2)
+    End Sub
 End Class
